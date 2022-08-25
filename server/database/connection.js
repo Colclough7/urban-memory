a4899c8fec10de1ec45e98e1c1427f21ee75a30e
Colclough7 <you@example.com>
2022-08-25 04:38:11 -0400

    db fix

--- server/database/connection.js
+++ server/database/connection.js
@@ -6,7 +6,8 @@ export const connectDB = async () => {
     try{
         // mongodb connection string
         const con = await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
 
         console.log(`MongoDB connected : ${con.connection.host}`);
